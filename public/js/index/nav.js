89d08108b86a15f4695c16c996c1c60409bf787a
DreiYan <dreYan823@gmail.com>
2018-11-28 18:14:12 +0800

    sign in created

--- public/js/index/nav.js
+++ public/js/index/nav.js
@@ -1,12 +1,23 @@
 let menuBtn = document.querySelector('.menu-btn')
 let menuBar = document.querySelector('.menu-bar')
+let signModal = document.querySelector('.sign-outer')
 
 menuBtn.addEventListener('click', () => {
     menuBar.style.width = "200px"
 })
 
 document.body.addEventListener('click', (e) => {
-    let className = e.target.className
-    if(className !== 'menu-bar' && className !== 'menu-btn')
+    let classList = e.target.classList
+
+    if(classList.contains('col-3') || classList.contains('col-12') || classList.value === '' || classList.contains('sign-modal'))
         menuBar.style.width = "0"
+        
+    if(classList.contains('sign-modal'))
+        signModal.style.display = "block"
+
+    if(classList.contains('close-icon'))
+        signModal.style.display = "none"
+
+    console.log(classList)
+
 })
